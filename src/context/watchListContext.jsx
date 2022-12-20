2ca9f2eac58a98ff19a6e35d8900608ca17d090a
Marcelo Petry <marcelospetry@gmail.com>
2022-12-20 19:34:30 -0300

    feat: context & provider component are done

---
+++ src/context/watchListContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const WatchListContext = createContext();
+
+export const WatchListContextProvider = (props) => {
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+
+  return (
+    <WatchListContext.Provider
+      value={{ watchList }}
+    ></WatchListContext.Provider>
+  );
+};
